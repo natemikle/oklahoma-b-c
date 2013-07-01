ced3db25ec958d6997b92dd18d189f28cc39274f
Nate M <natemikle@gmail.com>
2013-07-01 15:32:44 -0400

    added something

--- add_numb.py
+++ add_numb.py
@@ -1,8 +1,11 @@
-def add_two(a,b):
-   return a+b
+
+#authored by me
+from adding import add_two, add_three
 
 def main():
+   print ("2 + 2 + 2 = ", add_three (2,2,2))
    print ("2 + 3 = ",add_two(2,3))
+   print ("hello Oklahoma")
 
 if __name__ == "__main__":
      main()
